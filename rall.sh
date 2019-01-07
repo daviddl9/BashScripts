c38f582b743ef660a9543c2f6c28a018f9e784a1
David Livingston <ddl.tdh@gmail.com>
2019-01-07 19:12:00 +0800

    first

---
+++ rall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Compiles source and executes test cases
+# Usage: rall.sh <source file name> <test case prefix name>
+# Remember to give permissions i.e chmod u+x rall.sh
+# This script has to be located in the same directory as the skeletal code, 
+# with the input and output directories inside this directories.
+# e.g. rall.sh box_volume.c box 
+
+set -e # Exit on error
+
+sourcename=$1
+binname="${sourcename%.*}"
+testprefix=$2
+
+echo "Compiling source file $sourcename to $binname"
+
+javac $sourcename
+
+i=0
+while
+    ((i++))
+    testinfile="input/$testprefix$i.in"
+    [ -f $testinfile ]
+do
+    echo "Running test $testinfile"
+    outfile="test$i.out"
+    java $binname < $testinfile > $outfile
+    diff $outfile "output/$testprefix$i.out"
+done
+
+echo "Test $testprefix$i.in not found"
